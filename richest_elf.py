95d22e24611e6d0570dd8b5d832e22e196c483c8
Graham Home <grahamhome333@gmail.com>
2022-12-01 21:51:50 -0500

    Day 1

---
+++ richest_elf.py
@@ -0,0 +1,14 @@
+import sys
+
+def richest_elf(filepath):
+	max_cals = 0
+	current = 0
+	for item in map(lambda i: i.strip(), open(filepath).readlines()):
+		if item != "":
+			current += int(item)
+		else:
+			max_cals = max(max_cals, current)
+			current = 0
+	return max(max_cals, current)
+	
+print(richest_elf(sys.argv[1]))
