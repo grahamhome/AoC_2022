13dd5d352888e3f691224d40f10b635267df68ac
Graham Home <grahamhome333@gmail.com>
2022-12-02 19:07:25 -0500

    Day 2

---
+++ rps.py
@@ -0,0 +1,29 @@
+import sys
+
+p2_scores = {
+    "A": {
+        "X": 1 + 3,
+        "Y": 2 + 6,
+        "Z": 3 + 0,
+    },
+    "B": {
+        "X": 1 + 0,
+        "Y": 2 + 3,
+        "Z": 3 + 6,
+    },
+    "C": {
+        "X": 1 + 6,
+        "Y": 2 + 0,
+        "Z": 3 + 3,
+    }
+}
+
+
+def score_from_strategy(filepath):
+    total = 0
+    for p1_move, p2_move in map(lambda move: move.strip().split(" "), open(filepath, "r").readlines()):
+        total += p2_scores[p1_move][p2_move]
+    return total
+
+
+print(score_from_strategy(sys.argv[1]))
---
+++ rps_2.py
@@ -0,0 +1,29 @@
+import sys
+
+p2_scores = {
+    "A": {
+        "X": 3 + 0,
+        "Y": 1 + 3,
+        "Z": 2 + 6,
+    },
+    "B": {
+        "X": 1 + 0,
+        "Y": 2 + 3,
+        "Z": 3 + 6,
+    },
+    "C": {
+        "X": 2 + 0,
+        "Y": 3 + 3,
+        "Z": 1 + 6,
+    }
+}
+
+
+def score_from_strategy(filepath):
+    total = 0
+    for p1_move, p2_move in map(lambda move: move.strip().split(" "), open(filepath, "r").readlines()):
+        total += p2_scores[p1_move][p2_move]
+    return total
+
+
+print(score_from_strategy(sys.argv[1]))
