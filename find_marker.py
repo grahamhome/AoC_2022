10073602fbc228d0de16a417243c4d8ad2db525e
Graham Home <grahamhome333@gmail.com>
2022-12-06 09:08:11 -0500

    Day 6

--- find_marker.py
+++ find_marker.py
@@ -3,12 +3,10 @@ import sys
 
 def find_marker_index(filepath, marker_size):
     data = open(filepath, "r").readlines()[0].strip()
-    index_low = 0
-    index_high = marker_size
-    while not len(set(data[index_low:index_high])) == marker_size:
-        index_low += 1
-        index_high += 1
-    return index_high
+    index = marker_size
+    while not len(set(data[index - marker_size : index])) == marker_size:
+        index += 1
+    return index
 
 
 if __name__ == "__main__":
